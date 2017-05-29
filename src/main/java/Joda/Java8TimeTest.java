28c640910aef80c33fed726df49cfc6c680bed31
yinhao <yinhao@jd.com>
2017-05-29 14:13:05 +0800

    lesson 50

--- src/main/java/Joda/Java8TimeTest.java
+++ src/main/java/Joda/Java8TimeTest.java
@@ -1,8 +1,9 @@
 package Joda;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.MonthDay;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Created by yinhao on 17/5/28.
@@ -40,6 +41,77 @@ public class Java8TimeTest {
         //增加3小时，20分钟
         LocalDateTime time1 = time.plusHours(3).plusMinutes(20);
         System.out.println(time1);
+
+        System.out.println("----------");
+
+        LocalDate localDate3 = LocalDate.now();
+        System.out.println(localDate3.plus(2, ChronoUnit.WEEKS));//增加两周
+
+        System.out.println("----------");
+
+        System.out.println(localDate.minus(2,ChronoUnit.WEEKS));//后退两周
+
+        System.out.println("----------");
+
+        Clock clock = Clock.systemDefaultZone();
+        System.out.println(clock.millis());
+
+        System.out.println("----------");
+
+        LocalDate localDate4 = LocalDate.now();
+        LocalDate localDate5 = LocalDate.of(2017,12,12);
+        System.out.println(localDate4.isAfter(localDate5));
+        System.out.println(localDate4.isBefore(localDate5));
+        System.out.println(localDate4.equals(localDate5));
+
+        System.out.println("----------");
+
+        Set<String> availableZoneIds = ZoneId.getAvailableZoneIds();
+        availableZoneIds.stream().forEach(System.out::println);//所有时区 地区/地名
+
+        System.out.println("----------");
+
+        TreeSet<String> treeSet = new TreeSet<>();
+        treeSet.addAll(availableZoneIds);
+        treeSet.stream().forEach(System.out::println);//所有时区 排序
+
+        System.out.println("----------");
+
+        ZoneId zoneId = ZoneId.of("Asia/Shanghai");
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);//2017-05-28T21:01:02.864 本地时间
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(localDateTime,zoneId);
+        System.out.println(zonedDateTime);//2017-05-28T21:01:02.864+08:00[Asia/Shanghai] 带时区的时间
+
+        System.out.println("----------");
+
+        YearMonth yearMonth = YearMonth.now();
+        System.out.println(yearMonth);
+        System.out.println(yearMonth.lengthOfMonth());//天数
+        System.out.println(yearMonth.isLeapYear());//是不是闰年
+
+        System.out.println("----------");
+
+        YearMonth yearMonth1 = YearMonth.of(2016,2);
+        System.out.println(yearMonth1);
+        System.out.println(yearMonth1.lengthOfMonth());//天数
+        System.out.println(yearMonth1.lengthOfYear());//天数
+        System.out.println(yearMonth1.isLeapYear());//是不是闰年
+
+        System.out.println("----------");
+
+        LocalDate localDate6 = LocalDate.now();
+        LocalDate localDate7 = LocalDate.of(2018,5,29);
+        Period period = Period.between(localDate6,localDate7);
+        System.out.println(period.getYears());//间隔年数
+        System.out.println(period.getMonths());//间隔月数 只比较月数
+        System.out.println(period.getDays());//间隔天数 只比较天
+
+        System.out.println("----------");
+
+        System.out.println(Instant.now());//标准UTC时间 不带时区 北京时间减8小时
+
+
 
     }
 }
